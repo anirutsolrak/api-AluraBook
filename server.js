2419b8ee026406a908b61113ded64f65dd3b9401
anirutsolrak <146773453+anirutsolrak@users.noreply.github.com>
2024-05-24 17:42:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,27 +7,17 @@ const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/*": "/$1",
- })
-);
-server.use(router);
 
-// Definindo uma rota para lidar com requisições inválidas
-server.use((req, res, next) => {
-  if (req.method === 'GET' && req.url === '/') {
-    res.status(404).json({ error: 'Rota não encontrada' });
-  } else {
-    next(); // Prossegue para a próxima middleware
-  }
-});
+// Redirecionando todas as requisições GET para /produtos
+server.use(jsonServer.rewriter({
+  '/': '/produtos' 
+}));
+
+server.use(router);
 
 // Listen to port
 server.listen(3000, () => {
- console.log("JSON Server is running");
+  console.log("JSON Server is running");
 });
 
 // Export the Server API
